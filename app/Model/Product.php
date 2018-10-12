211e4e4544aed21b39644bc42f1577a6cbc320c1
Mohamed El Mahdy <mohamedelmahdymostafa@gmail.com>
2018-10-13 01:57:07 +0200

    RelationShips

--- app/Model/Product.php
+++ app/Model/Product.php
@@ -9,4 +9,9 @@ class Product extends Model
     protected $fillable = [
         'name','detail','price','stock','discount'
     ];
+
+    public function reviews()
+    {
+        return $this->hasMany(Review::Class);
+    }
 }
